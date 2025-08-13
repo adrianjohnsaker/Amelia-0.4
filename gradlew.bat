0e246d0362c6e21fbbf94233be73213274774cf8
adrianjohnsaker <adriansaker@outlook.com>
2025-08-13 09:05:34 +0100

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,32 +1,16 @@
-@if "%DEBUG%" == "" @echo off
-@rem ##########################################################################
-@rem  Gradle startup script for Windows
-@rem ##########################################################################
+@echo off
+SETLOCAL
 
-set DIRNAME=%~dp0
-if "%DIRNAME%" == "" set DIRNAME=.
-set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%
+SET PRG=%~dpnx0
+SET PRGDIR=%~dp0
+SET CLASSPATH=%PRGDIR%gradle\wrapper\gradle-wrapper.jar
 
-set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+IF NOT DEFINED JAVA_HOME (
+  SET JAVA_EXE=java
+) ELSE (
+  SET JAVA_EXE=%JAVA_HOME%\bin\java.exe
+)
 
-set JAVA_EXE=java.exe
-%JAVA_EXE% -version >NUL 2>&1
-if "%ERRORLEVEL%" == "0" goto execute
+"%JAVA_EXE%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
 
-echo ERROR: JAVA_HOME is not set
-goto fail
-
-:execute
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
-
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
-
-:end
-if "%ERRORLEVEL%"=="0" goto mainEnd
-
-:fail
-exit /b 1
-
-:mainEnd
-if "%OS%"=="Windows_NT" endlocal
+ENDLOCAL
